d99b91af9eacf06469a6875b6a9577bb14026c9a
Klevtsov Stanislav <klevtsovstanislav@gmail.com>
2018-05-15 01:55:32 +0300

    Added

--- Go/src/main/main.go
+++ Go/src/main/main.go
@@ -7,7 +7,7 @@ import ("fmt"
 func main(){
   fmt.Println("Test")
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w,"hello world")
+    fmt.Fprintf(w,"This is my localhost")
   })
 
   http.ListenAndServe(":8080",nil)
